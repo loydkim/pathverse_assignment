f3428447aaec669467712bc677a9dd29b0cb3ab1
Loydkim <kimloydmail@gmail.com>
2023-03-03 15:44:46 -0800

    add mail vaildate

--- pathverse_loyd/lib/pages/login/login.dart
+++ pathverse_loyd/lib/pages/login/login.dart
@@ -9,6 +9,27 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   bool _passwordVisible = false;
+  final _formKey = GlobalKey<FormState>();
+
+  String? validateEmail(String? value) {
+    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
+    if (value == null || value.isEmpty) {
+      return "This field can't be empty!";
+    } else if (value.isNotEmpty && !regex.hasMatch(value)) {
+      return 'Enter a valid email address';
+    } else {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +59,7 @@ class _LoginState extends State<Login> {
                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: Row(
                       children: [
-                        Text("Let's sign you in.",
+                        const Text("Let's sign you in.",
                             style:
                                 TextStyle(color: Colors.white, fontSize: 30)),
                         const SizedBox(
@@ -59,96 +80,159 @@ class _LoginState extends State<Login> {
                       width: double.infinity,
                       decoration: BoxDecoration(
                           color: Colors.white.withOpacity(0.2),
-                          borderRadius: BorderRadius.only(
+                          borderRadius: const BorderRadius.only(
                               topLeft: Radius.circular(14),
                               topRight: Radius.circular(14))),
                       child: Padding(
                         padding: const EdgeInsets.symmetric(
                             horizontal: 30, vertical: 36),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            TextFormField(
-                              decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                  borderSide: BorderSide(
-                                    color: Colors.white.withOpacity(0.4),
-                                    width: 2.0,
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextFormField(
+                                  decoration: InputDecoration(
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      borderSide: BorderSide(
+                                        color: Colors.white.withOpacity(0.4),
+                                        width: 2.0,
+                                      ),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      borderSide: BorderSide(
+                                        color: Colors.white.withOpacity(0.4),
+                                        // width: 1.0,
+                                      ),
+                                    ),
+                                    errorBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      borderSide: BorderSide(
+                                        color: Colors.red.withOpacity(0.4),
+                                        width: 2.4,
+                                      ),
+                                    ),
+                                    focusedErrorBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      borderSide: BorderSide(
+                                        color: Colors.red.withOpacity(0.4),
+                                        width: 2.4,
+                                      ),
+                                    ),
+                                    labelText: "Email Address",
+                                    fillColor: Colors.white.withOpacity(0.2),
+                                    filled: true,
+                                    contentPadding: const EdgeInsets.fromLTRB(
+                                        16, 9.5, 16, 9.5),
+                                    labelStyle: TextStyle(
+                                        color: Colors.white.withOpacity(0.4)),
+                                    errorStyle: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.red.withOpacity(0.6),
+                                    ),
                                   ),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                  borderSide: BorderSide(
-                                    color: Colors.white.withOpacity(0.4),
-                                    // width: 1.0,
-                                  ),
-                                ),
-                                labelText: "Email Address",
-                                fillColor: Colors.white.withOpacity(0.2),
-                                filled: true,
-                                contentPadding:
-                                    const EdgeInsets.fromLTRB(16, 9.5, 16, 9.5),
-                                labelStyle: TextStyle(
-                                    color: Colors.white.withOpacity(0.4)),
+                                  style: TextStyle(
+                                      color: Colors.white.withOpacity(0.8)),
+                                  validator: validateEmail),
+                              const SizedBox(
+                                height: 20,
                               ),
-                              style: TextStyle(
-                                  color: Colors.white.withOpacity(0.8)),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(5.0),
-                                    borderSide: BorderSide(
-                                      color: Colors.white.withOpacity(0.4),
-                                      width: 2.0,
-                                    ),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(5.0),
-                                    borderSide: BorderSide(
-                                      color: Colors.white.withOpacity(0.4),
-                                      // width: 1.0,
-                                    ),
-                                  ),
-                                  labelText: "Password",
-                                  fillColor: Colors.white.withOpacity(0.2),
-                                  filled: true,
-                                  contentPadding: const EdgeInsets.fromLTRB(
-                                      16, 9.5, 16, 9.5),
-                                  labelStyle: TextStyle(
-                                    color: Colors.white.withOpacity(0.4),
-                                  ),
-                                  suffixIcon: IconButton(
-                                    icon: Icon(
-                                      _passwordVisible
-                                          ? Icons.visibility
-                                          : Icons.visibility_off,
-                                      color: Colors.white.withOpacity(0.6),
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        _passwordVisible = !_passwordVisible;
-                                      });
-                                    },
-                                  )),
-                              style: TextStyle(
-                                  color: Colors.white.withOpacity(0.8)),
-                              obscureText: !_passwordVisible,
-                            ),
-                            SizedBox(
-                              height: 40,
-                            ),
-                            Container(
-                              width: double.infinity,
-                              height: 40,
-                              child: ElevatedButton(
-                                  onPressed: () => {}, child: Text("Log in")),
-                            )
-                          ],
+                              TextFormField(
+                                  decoration: InputDecoration(
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0),
+                                        borderSide: BorderSide(
+                                          color: Colors.white.withOpacity(0.4),
+                                          width: 2.0,
+                                        ),
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0),
+                                        borderSide: BorderSide(
+                                          color: Colors.white.withOpacity(0.4),
+                                          // width: 1.0,
+                                        ),
+                                      ),
+                                      errorBorder: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0),
+                                        borderSide: BorderSide(
+                                          color: Colors.red.withOpacity(0.4),
+                                          width: 2.4,
+                                        ),
+                                      ),
+                                      focusedErrorBorder: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0),
+                                        borderSide: BorderSide(
+                                          color: Colors.red.withOpacity(0.4),
+                                          width: 2.4,
+                                        ),
+                                      ),
+                                      labelText: "Password",
+                                      fillColor: Colors.white.withOpacity(0.2),
+                                      filled: true,
+                                      contentPadding: const EdgeInsets.fromLTRB(
+                                          16, 9.5, 16, 9.5),
+                                      labelStyle: TextStyle(
+                                        color: Colors.white.withOpacity(0.4),
+                                      ),
+                                      errorStyle: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.red.withOpacity(0.6),
+                                      ),
+                                      suffixIcon: IconButton(
+                                        icon: Icon(
+                                          _passwordVisible
+                                              ? Icons.visibility
+                                              : Icons.visibility_off,
+                                          color: Colors.white.withOpacity(0.6),
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            _passwordVisible =
+                                                !_passwordVisible;
+                                          });
+                                        },
+                                      )),
+                                  style: TextStyle(
+                                      color: Colors.white.withOpacity(0.8)),
+                                  obscureText: !_passwordVisible,
+                                  validator: (String? value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "This field can't be empty!";
+                                    }
+                                    return null;
+                                  }),
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 40,
+                                child: ElevatedButton(
+                                    onPressed: () => {
+                                          // Validate returns true if the form is valid, or false otherwise.
+                                          if (_formKey.currentState!.validate())
+                                            {
+                                              // If the form is valid, display a snackbar. In the real world,
+                                              // you'd often call a server or save the information in a database.
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(
+                                                const SnackBar(
+                                                    content: Text(
+                                                        'Processing Data')),
+                                              )
+                                            }
+                                        },
+                                    child: Text("Log in")),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
