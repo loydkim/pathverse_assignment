30dbe13cb7d3cb60201f6a003e2de1d7ccc7539c
Loydkim <kimloydmail@gmail.com>
2023-03-08 09:45:44 -0800

    update splash image on iOS. add sliver app bar

--- pathverse_loyd/lib/pages/comment/comment_page.dart
+++ pathverse_loyd/lib/pages/comment/comment_page.dart
@@ -12,120 +12,133 @@ class CommentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo.shade800,
-        foregroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.share),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-          color: Colors.indigo.shade800,
-          width: double.infinity,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.indigo.shade800,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          post.title,
-                          style: AppTextTheme.styleW700
-                              .copyWith(fontSize: 20, color: Colors.white),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          post.body,
-                          style: AppTextTheme.styleW300
-                              .copyWith(fontSize: 16, color: Colors.white),
-                        ),
-                      ],
-                    ),
+      body: FutureBuilder(
+          future: PostsAPIProvider().loadComments(post.id),
+          builder: (context, snapshot) {
+            Widget commentListSliver;
+
+            if (snapshot.hasError) {
+              commentListSliver = const SliverToBoxAdapter(
+                child: Center(
+                  child: Text('An error has occurred!'),
+                ),
+              );
+            } else if (snapshot.hasData) {
+              if (snapshot.data!.isEmpty) {
+                commentListSliver = const SliverToBoxAdapter(
+                    child: Center(child: Text("No Comments")));
+              }
+              List<Comment> comments = snapshot.data!;
+              commentListSliver = SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                return CommentItem(
+                  comment: comments[index],
+                );
+              }, childCount: comments.length));
+            } else {
+              commentListSliver = SliverToBoxAdapter(
+                child: Container(
+                  color: Colors.white.withOpacity(0.93),
+                  height: 500,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
                   ),
                 ),
-                const SizedBox(
-                  height: 18,
-                ),
-                Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.93),
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(22),
-                          topRight: Radius.circular(22))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
+              );
+            }
+
+            return Container(
+              color: Colors.indigo.shade800,
+              child: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    floating: true,
+                    backgroundColor: Colors.indigo.shade800,
+                    foregroundColor: Colors.white,
+                    elevation: 0,
+                    actions: [
+                      IconButton(
+                        icon: const Icon(Icons.share),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  SliverToBoxAdapter(
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              const Icon(
-                                Icons.edit_note,
-                                color: Colors.black54,
-                                size: 28,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                "All Comments",
-                                style: AppTextTheme.styleW700.copyWith(
-                                  fontSize: 24,
-                                  color: Colors.black87,
+                        Container(
+                          color: Colors.indigo.shade800,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  post.title,
+                                  style: AppTextTheme.styleW700.copyWith(
+                                      fontSize: 20, color: Colors.white),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  post.body,
+                                  style: AppTextTheme.styleW300.copyWith(
+                                      fontSize: 16, color: Colors.white),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                         const SizedBox(
-                          height: 10,
+                          height: 18,
                         ),
-                        FutureBuilder(
-                          future: PostsAPIProvider().loadComments(post.id),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              return const Center(
-                                child: Text('An error has occurred!'),
-                              );
-                            } else if (snapshot.hasData) {
-                              if (snapshot.data!.isEmpty) {
-                                return const Center(child: Text("No Comments"));
-                              }
-
-                              return Column(
-                                children: snapshot.data!
-                                    .map((comment) =>
-                                        CommentItem(comment: comment))
-                                    .toList(),
-                              );
-                            } else {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
-                          },
+                        Container(
+                          width: double.infinity,
+                          height: 22,
+                          decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(0.93),
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(22),
+                                  topRight: Radius.circular(22))),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 46,
+                          color: Colors.white.withOpacity(0.93),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 14),
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.edit_note,
+                                  color: Colors.black54,
+                                  size: 28,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "All Comments",
+                                  style: AppTextTheme.styleW700.copyWith(
+                                    fontSize: 24,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          )),
+                  commentListSliver
+                ],
+              ),
+            );
+          }),
     );
   }
 }
--- pathverse_loyd/lib/pages/user/user_page.dart
+++ pathverse_loyd/lib/pages/user/user_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pathverse_loyd/common/theme/app_text_theme.dart';
-import 'package:pathverse_loyd/common/widgets/custom_app_bar.dart';
 import 'package:pathverse_loyd/common/widgets/post_item.dart';
+import 'package:pathverse_loyd/models/post.dart';
 import 'package:pathverse_loyd/provider/users_api_provider.dart';
 
 class UserPage extends StatelessWidget {
@@ -11,70 +11,94 @@ class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar.userPageAppBar(userId: userId),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.edit_calendar,
-                      size: 28,
-                      color: Colors.black54,
+      body: FutureBuilder(
+          future: UsersAPIProvider().loadUserPosts(userId),
+          builder: (context, snapshot) {
+            Widget recentPostListSliver;
+
+            if (snapshot.hasError) {
+              recentPostListSliver = const SliverToBoxAdapter(
+                child: Center(
+                  child: Text('An error has occurred!'),
+                ),
+              );
+            } else if (snapshot.hasData) {
+              if (snapshot.data!.isEmpty) {
+                recentPostListSliver = const SliverToBoxAdapter(
+                    child: Center(child: Text("No Posts")));
+              }
+              List<Post> posts = snapshot.data!;
+              recentPostListSliver = SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                return PostItem(
+                  post: posts[index],
+                  isShowUserName: false,
+                );
+              }, childCount: posts.length));
+            } else {
+              recentPostListSliver = const SliverToBoxAdapter(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  floating: true,
+                  expandedHeight: 60.0,
+                  shape: const ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40))),
+                  backgroundColor: Colors.indigo.shade800,
+                  foregroundColor: Colors.white,
+                  title: Text("User$userId"),
+                  centerTitle: false,
+                  actions: [
+                    IconButton(
+                      icon: const Icon(Icons.info),
+                      onPressed: () {},
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text("Recent Posts",
-                        style: AppTextTheme.styleW700.copyWith(
-                          fontSize: 24,
-                          color: Colors.black87,
-                        )),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              FutureBuilder(
-                future: UsersAPIProvider().loadUserPosts(userId),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return const Center(
-                      child: Text('An error has occurred!'),
-                    );
-                  } else if (snapshot.hasData) {
-                    if (snapshot.data!.isEmpty) {
-                      return const Center(child: Text("No Posts"));
-                    }
-
-                    return Column(
-                      children: snapshot.data!
-                          .map((post) => PostItem(
-                                post: post,
-                                isShowUserName: false,
-                              ))
-                          .toList(),
-                    );
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
-            ]),
-          ),
-        ),
-      ),
+                SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.edit_calendar,
+                              size: 28,
+                              color: Colors.black54,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text("Recent Posts",
+                                style: AppTextTheme.styleW700.copyWith(
+                                  fontSize: 24,
+                                  color: Colors.black87,
+                                )),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                    ],
+                  ),
+                ),
+                recentPostListSliver
+              ],
+            );
+          }),
     );
   }
 }
